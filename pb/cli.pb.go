2219f935dd1f1bbde18e7cae5a891e19398fbefd
Akashi <mohitsilwal16@gmail.com>
2025-06-21 04:45:44 +0530

    Slight Changes in Models

--- pb/cli.pb.go
+++ pb/cli.pb.go
@@ -323,7 +323,7 @@ type RequestPunchFromReceiverRequest struct {
 	ListenerPassword       string                 `protobuf:"bytes,2,opt,name=listener_password,json=listenerPassword,proto3" json:"listener_password,omitempty"`
 	ListenerPublicIp       string                 `protobuf:"bytes,3,opt,name=listener_public_ip,json=listenerPublicIp,proto3" json:"listener_public_ip,omitempty"`
 	ListenerPublicPort     string                 `protobuf:"bytes,4,opt,name=listener_public_port,json=listenerPublicPort,proto3" json:"listener_public_port,omitempty"`
-	ListenerPrivateIp      []string               `protobuf:"bytes,5,rep,name=listener_private_ip,json=listenerPrivateIp,proto3" json:"listener_private_ip,omitempty"`
+	ListenerPrivateIpList  []string               `protobuf:"bytes,5,rep,name=listener_private_ip_list,json=listenerPrivateIpList,proto3" json:"listener_private_ip_list,omitempty"`
 	ListenerPrivatePort    string                 `protobuf:"bytes,6,opt,name=listener_private_port,json=listenerPrivatePort,proto3" json:"listener_private_port,omitempty"`
 	WorkspaceOwnerUsername string                 `protobuf:"bytes,7,opt,name=workspace_owner_username,json=workspaceOwnerUsername,proto3" json:"workspace_owner_username,omitempty"`
 	unknownFields          protoimpl.UnknownFields
@@ -388,9 +388,9 @@ func (x *RequestPunchFromReceiverRequest) GetListenerPublicPort() string {
 	return ""
 }
 
-func (x *RequestPunchFromReceiverRequest) GetListenerPrivateIp() []string {
+func (x *RequestPunchFromReceiverRequest) GetListenerPrivateIpList() []string {
 	if x != nil {
-		return x.ListenerPrivateIp
+		return x.ListenerPrivateIpList
 	}
 	return nil
 }
@@ -410,13 +410,13 @@ func (x *RequestPunchFromReceiverRequest) GetWorkspaceOwnerUsername() string {
 }
 
 type RequestPunchFromReceiverResponse struct {
-	state                     protoimpl.MessageState `protogen:"open.v1"`
-	WorkspaceOwnerPublicIp    string                 `protobuf:"bytes,1,opt,name=workspace_owner_public_ip,json=workspaceOwnerPublicIp,proto3" json:"workspace_owner_public_ip,omitempty"`
-	WorkspaceOwnerPublicPort  string                 `protobuf:"bytes,2,opt,name=workspace_owner_public_port,json=workspaceOwnerPublicPort,proto3" json:"workspace_owner_public_port,omitempty"`
-	WorkspaceOwnerPrivateIp   []string               `protobuf:"bytes,3,rep,name=workspace_owner_private_ip,json=workspaceOwnerPrivateIp,proto3" json:"workspace_owner_private_ip,omitempty"`
-	WorkspaceOwnerPrivatePort string                 `protobuf:"bytes,4,opt,name=workspace_owner_private_port,json=workspaceOwnerPrivatePort,proto3" json:"workspace_owner_private_port,omitempty"`
-	unknownFields             protoimpl.UnknownFields
-	sizeCache                 protoimpl.SizeCache
+	state                       protoimpl.MessageState `protogen:"open.v1"`
+	WorkspaceOwnerPublicIp      string                 `protobuf:"bytes,1,opt,name=workspace_owner_public_ip,json=workspaceOwnerPublicIp,proto3" json:"workspace_owner_public_ip,omitempty"`
+	WorkspaceOwnerPublicPort    string                 `protobuf:"bytes,2,opt,name=workspace_owner_public_port,json=workspaceOwnerPublicPort,proto3" json:"workspace_owner_public_port,omitempty"`
+	WorkspaceOwnerPrivateIpList []string               `protobuf:"bytes,3,rep,name=workspace_owner_private_ip_list,json=workspaceOwnerPrivateIpList,proto3" json:"workspace_owner_private_ip_list,omitempty"`
+	WorkspaceOwnerPrivatePort   string                 `protobuf:"bytes,4,opt,name=workspace_owner_private_port,json=workspaceOwnerPrivatePort,proto3" json:"workspace_owner_private_port,omitempty"`
+	unknownFields               protoimpl.UnknownFields
+	sizeCache                   protoimpl.SizeCache
 }
 
 func (x *RequestPunchFromReceiverResponse) Reset() {
@@ -463,9 +463,9 @@ func (x *RequestPunchFromReceiverResponse) GetWorkspaceOwnerPublicPort() string 
 	return ""
 }
 
-func (x *RequestPunchFromReceiverResponse) GetWorkspaceOwnerPrivateIp() []string {
+func (x *RequestPunchFromReceiverResponse) GetWorkspaceOwnerPrivateIpList() []string {
 	if x != nil {
-		return x.WorkspaceOwnerPrivateIp
+		return x.WorkspaceOwnerPrivateIpList
 	}
 	return nil
 }
@@ -861,19 +861,19 @@ const file_proto_cli_proto_rawDesc = "" +
 	"\x11listener_password\x18\x02 \x01(\tR\x10listenerPassword\x12%\n" +
 	"\x0eworkspace_name\x18\x03 \x01(\tR\rworkspaceName\x128\n" +
 	"\x18workspace_owner_username\x18\x04 \x01(\tR\x16workspaceOwnerUsername\"!\n" +
-	"\x1fRegisterUserToWorkspaceResponse\"\xf9\x02\n" +
+	"\x1fRegisterUserToWorkspaceResponse\"\x82\x03\n" +
 	"\x1fRequestPunchFromReceiverRequest\x12+\n" +
 	"\x11listener_username\x18\x01 \x01(\tR\x10listenerUsername\x12+\n" +
 	"\x11listener_password\x18\x02 \x01(\tR\x10listenerPassword\x12,\n" +
 	"\x12listener_public_ip\x18\x03 \x01(\tR\x10listenerPublicIp\x120\n" +
-	"\x14listener_public_port\x18\x04 \x01(\tR\x12listenerPublicPort\x12.\n" +
-	"\x13listener_private_ip\x18\x05 \x03(\tR\x11listenerPrivateIp\x122\n" +
+	"\x14listener_public_port\x18\x04 \x01(\tR\x12listenerPublicPort\x127\n" +
+	"\x18listener_private_ip_list\x18\x05 \x03(\tR\x15listenerPrivateIpList\x122\n" +
 	"\x15listener_private_port\x18\x06 \x01(\tR\x13listenerPrivatePort\x128\n" +
-	"\x18workspace_owner_username\x18\a \x01(\tR\x16workspaceOwnerUsername\"\x9a\x02\n" +
+	"\x18workspace_owner_username\x18\a \x01(\tR\x16workspaceOwnerUsername\"\xa3\x02\n" +
 	" RequestPunchFromReceiverResponse\x129\n" +
 	"\x19workspace_owner_public_ip\x18\x01 \x01(\tR\x16workspaceOwnerPublicIp\x12=\n" +
-	"\x1bworkspace_owner_public_port\x18\x02 \x01(\tR\x18workspaceOwnerPublicPort\x12;\n" +
-	"\x1aworkspace_owner_private_ip\x18\x03 \x03(\tR\x17workspaceOwnerPrivateIp\x12?\n" +
+	"\x1bworkspace_owner_public_port\x18\x02 \x01(\tR\x18workspaceOwnerPublicPort\x12D\n" +
+	"\x1fworkspace_owner_private_ip_list\x18\x03 \x03(\tR\x1bworkspaceOwnerPrivateIpList\x12?\n" +
 	"\x1cworkspace_owner_private_port\x18\x04 \x01(\tR\x19workspaceOwnerPrivatePort\"\xf1\x01\n" +
 	"\x1fNotifyNewPushToListenersRequest\x128\n" +
 	"\x18workspace_owner_username\x18\x01 \x01(\tR\x16workspaceOwnerUsername\x128\n" +
