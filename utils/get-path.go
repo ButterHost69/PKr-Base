dd706749a5690d489535136e0c9b21e2cecbcc66
ButterHost69 <butterhost069@gmail.com>
2025-07-22 08:39:22 +0530

    Force PKr-Base to use custom Config Filepath

--- utils/get-path.go
+++ utils/get-path.go
@@ -8,28 +8,48 @@ import (
 	"runtime"
 )
 
-func GetUserConfigRootDir() (string, error) {
-	var base_dir string
+var USER_CONFIF_FILE_DIR string
 
-	current_user, err := user.Current()
-	if err != nil {
-		fmt.Println("Error while Getting Current User:", err)
-		fmt.Println("Source: GetUserConfigRootDir()")
-		return "", err
-	}
-
-	switch runtime.GOOS {
-	case "windows":
-		base_dir = os.Getenv("LOCALAPPDATA") // Typically C:\Users\<User>\AppData\Local
-	case "darwin":
-		base_dir = filepath.Join(current_user.HomeDir, "Library", "Application Support")
-	default: // Linux and other Unix-like systems
-		base_dir = os.Getenv("XDG_DATA_HOME")
-		if base_dir == "" {
-			base_dir = filepath.Join(current_user.HomeDir, ".local", "share")
+func SetUserConfigDir(file_path string) error {
+	if file_path == "" {
+		var err error
+		USER_CONFIF_FILE_DIR, err = GetUserConfigRootDir()
+		if err != nil {
+			fmt.Println("Error while Getting Path of Local App Data:", err)
+			fmt.Println("Source: GetUserConfigFilePath()")
+			return err
 		}
 	}
-	return filepath.Join(base_dir, "PKr"), nil
+	USER_CONFIF_FILE_DIR = file_path
+	return nil
+}
+
+func GetUserConfigRootDir() (string, error) {
+	if USER_CONFIF_FILE_DIR == "" {
+		var base_dir string
+
+		current_user, err := user.Current()
+		if err != nil {
+			fmt.Println("Error while Getting Current User:", err)
+			fmt.Println("Source: GetUserConfigRootDir()")
+			return "", err
+		}
+
+		switch runtime.GOOS {
+		case "windows":
+			base_dir = os.Getenv("LOCALAPPDATA") // Typically C:\Users\<User>\AppData\Local
+		case "darwin":
+			base_dir = filepath.Join(current_user.HomeDir, "Library", "Application Support")
+		default: // Linux and other Unix-like systems
+			base_dir = os.Getenv("XDG_DATA_HOME")
+			if base_dir == "" {
+				base_dir = filepath.Join(current_user.HomeDir, ".local", "share")
+			}
+		}
+		return filepath.Join(base_dir, "PKr"), nil
+	}
+	return USER_CONFIF_FILE_DIR, nil
+
 }
 
 func GetMyKeysPath() (string, error) {
